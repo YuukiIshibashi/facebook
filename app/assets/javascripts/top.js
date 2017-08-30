6038e3da3b2f23c6026224594bafea2fcab796ef
Yuukitanaka <robopowerful_vv@yahoo.co.jp>
2017-08-30 15:46:52 +0900

    js

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -2,6 +2,6 @@ $(function(){
 	$('.input-image').on('click', '[type=file]', function(e) {
 		var Value = e.currentTarget.attributes.class.value;
 		var inputValue = parseInt(Value) + 1;
-		$(".input-image").append(`<input type='file' name='topic[pictures_attributes][${inputValue}][image]' id='topic_pictures_attributes_${inputValue}_image' class='${inputValue}'>`);
+		$(".input-image").append("<input type='file' name='topic[pictures_attributes][" + inputValue + "][image]' id='topic_pictures_attributes_" + inputValue + "_image' class='" + inputValue + "'>");
 	});
 });
