b9663998d0523572973e123c132f2f6ee99e28e2
Yuukitanaka <robopowerful_vv@yahoo.co.jp>
2017-09-01 14:22:18 +0900

    修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery/dist/jquery.min
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
